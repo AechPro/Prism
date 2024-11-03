e23c871eee58b9c03b94a0e9f4e98b1c07b2dfd3
Matthew Allen <AechPro@users.noreply.github.com>
2024-11-03 15:45:27 -0500

    Fix import errors.

--- multiprocessing_experience_collection/__init__.py
+++ multiprocessing_experience_collection/__init__.py
@@ -1,4 +1,3 @@
 from .shared_memory_interface import SharedMemoryInterface, EnvProcessMemoryInterface
 from .collector_process_interface import CollectorProcessInterface
-from .experience_collector import MultiprocessExperienceCollector
 from .experience_collector import ExperienceCollector
